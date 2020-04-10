2031459db9c14ede247e1a44d6f000ffe145216b
Abhishek Kumar <abhi.mca.pu@gmail.com>
2020-04-10 11:29:39 +0530

    Add files via upload

---
+++ csv_to_dynamodb.py
@@ -0,0 +1,23 @@
+import boto3
+import pandas as pd
+
+data_frame=pd.read_csv("emp_data.csv",header=None)
+data_frame.columns=["emp_id","corp_id","emp_name","salary","currency","country"]
+
+for i in data_frame.columns:
+    data_frame[i]=data_frame[i].astype(str)
+
+#Convert dataframe to list of dictionaries(JSON) to be consumed by no-sql db
+json_list=data_frame.T.to_dict().values()
+
+print(json_list)
+
+#Convert to dynamodb using boto3
+dynamodb=boto3.resource('dynamodb',region_name='ap-south-1')
+
+#Connect to DynamoDB table
+table=dynamodb.Table("emp_data")
+
+#Load the JSON object using put_item method
+for student in json_list:
+    table.put_item(Item=student)
